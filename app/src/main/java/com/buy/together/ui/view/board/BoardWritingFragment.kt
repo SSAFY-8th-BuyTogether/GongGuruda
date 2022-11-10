1a431bf02dae621254954102f1a75199ec6d5f7b
Lee-Areum <arlee957790@gmail.com>
2022-11-10 17:51:59 +0900

    [FEATURE] Update boardWriting 화면 - Spinner 생성 - 화면 생성중

--- app/src/main/java/com/buy/together/ui/view/board/BoardWritingFragment.kt
+++ app/src/main/java/com/buy/together/ui/view/board/BoardWritingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.buy.together.R
 import com.buy.together.databinding.FragmentBoardBinding
 import com.buy.together.databinding.FragmentBoardWritingBinding
@@ -13,8 +14,16 @@ import com.buy.together.ui.base.BaseFragment
 class BoardWritingFragment : BaseFragment<FragmentBoardWritingBinding>(
     FragmentBoardWritingBinding::bind, R.layout.fragment_board_writing
 ){
+    lateinit var spinnerAdapter : ArrayAdapter<String>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setAdapter()
+    }
+
+    fun setAdapter(){
+        val items = resources.getStringArray(R.array.categories)
+        spinnerAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,items)
+        binding.spinnerCategory.adapter = spinnerAdapter
     }
 }
